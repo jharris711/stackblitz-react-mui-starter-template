4a92d1991bdb27f2a9ec39cb1074b3d01badfe5f
jharris711 <js.harris711@gmail.com>
2021-03-28 17:42:33 -0400

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import store from "./redux/store";
 import App from "./components/App";
 
+const theme = createMuiTheme({
+  palette: {
+    type: "dark"
+  }
+});
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById("root")
 );
